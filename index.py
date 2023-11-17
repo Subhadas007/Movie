bddd5aee7f0e07e871b280a33adca74b9228a4ef
Subhadas007 <151136252+Subhadas007@users.noreply.github.com>
2023-11-17 18:32:21 +0530

    Update index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("6521441565:AAFrt3fOPT5YdLKGrh8q4ZjOpXOVDEH1p3g")
-URL = os.getenv("URL")
+URL = os.getenv("https://yomovies.men/")
 bot = Bot(TOKEN)
 
 
